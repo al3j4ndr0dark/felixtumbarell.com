8d4d5d4745c75f525e4ccfaf5ef37b49abcc61e7
al3j4ndr0dark <81258757+al3j4ndr0dark@users.noreply.github.com>
2023-03-13 02:07:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+/* JavaScript for Smooth Scrolling */
+
+
+
+/*
+const title = document.querySelector(".title");
+const textVariants = ["I said you are wellcome", "wellcome enough to read", "continue reading"]; // add more variants here if needed
+
+let currentIndex = 10;
+let intervalId;
+
+function animateTitle() {
+  intervalId = setInterval(() => {
+    // get a random index different from the current one
+    let newIndex = currentIndex;
+    while (newIndex === currentIndex) {
+      newIndex = Math.floor(Math.random() * textVariants.length);
+    }
+    currentIndex = newIndex;
+    
+    // set the new text
+    title.textContent = textVariants[currentIndex];
+  }, 3000); // change the interval time here if needed
+}
+
+animateTitle();
+
+
+
+
+
+*/
+
+
+
+/* the typing effect */
+
+
+      
+
+
+
+
+
+  
